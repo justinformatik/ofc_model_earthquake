996a6e3468b68b9dbf6d2c9a13da237fdb13d23b
Justin <justinkowalski@gmx.de>
2025-12-31 13:09:49 -0500

    push

--- my_numerics.c
+++ my_numerics.c
@@ -3,7 +3,7 @@
 #include "my_numerics.h"
 #include <gsl/gsl_rng.h>
 
-//Dies ist die einzige Funktion, die für die Abgabe notwendig war. Diese hatte ich bereits vor der Prüfung geschrieben.
+
 gsl_rng* initGSL(){
     gsl_rng* myGenerator = gsl_rng_alloc(gsl_rng_mt19937); //Verwende Mersenne Twister 19937
     gsl_rng_set(myGenerator, time(NULL));                  //Verwende als Seed Zeit
